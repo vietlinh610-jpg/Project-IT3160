cf92427b2488a4a7198f547dfa6485fee7235db4
Bui Vuong <vuongbui72440@gmail.com>
2026-05-21 15:23:30 +0700

    chore: update node generation script for Moscow Metro dataset

--- getdata/laydulieunode.py
+++ getdata/laydulieunode.py
@@ -111,10 +111,10 @@ def interpolate_graph_edges(G, dist=50):
 # Hàm chính
 def main():
     # Lấy đồ thị từ OSM
-    # G = ox.graph_from_place("Truc Bach, Ba Dinh, Ha Noi", network_type="all", simplify=False, retain_all=True, truncate_by_edge=False)
+    # G = ox.graph_from_place("Moscow, Russia", network_type="all", simplify=False, retain_all=True, truncate_by_edge=False)
     
     # Lấy đồ thị có hướng từ OSM
-    G = ox.graph_from_place("Truc Bach, Ba Dinh, Ha Noi", network_type="all", simplify=False, retain_all=True, truncate_by_edge=False)
+    G = ox.graph_from_place("Moscow, Russia", network_type="all", simplify=False, retain_all=True, truncate_by_edge=False)
 
     # Chuyển đồ thị thành đồ thị có hướng
     # G = ox.utils_graph.get_directions(G)
